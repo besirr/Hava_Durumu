34fd2525a2880017a8feb342ba8842c16fc195c9
Mehmet Beşir <Mehmet Beir@BESIR>
2025-02-12 17:36:03 +0300

    Add project files.

---
+++ Hava_Durumu/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Hava_Durumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(21, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(239, 43);
+            label1.TabIndex = 0;
+            label1.Text = "İSTANBUL,TR";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(21, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 25);
+            label2.TabIndex = 1;
+            label2.Text = "İSTANBUL,TR";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(21, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Bugün";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft JhengHei UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(208, 237);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 60);
+            label4.TabIndex = 3;
+            label4.Text = "00";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, 172);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(140, 125);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(21, 314);
+            label5.Name = "label5";
+            label5.Size = new Size(119, 38);
+            label5.TabIndex = 5;
+            label5.Text = "Rüzgar";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(155, 314);
+            label6.Name = "label6";
+            label6.Size = new Size(119, 38);
+            label6.TabIndex = 6;
+            label6.Text = "Rüzgar";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(155, 373);
+            label7.Name = "label7";
+            label7.Size = new Size(119, 38);
+            label7.TabIndex = 8;
+            label7.Text = "Rüzgar";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(21, 373);
+            label8.Name = "label8";
+            label8.Size = new Size(88, 38);
+            label8.TabIndex = 7;
+            label8.Text = "Nem";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(155, 420);
+            label9.Name = "label9";
+            label9.Size = new Size(119, 38);
+            label9.TabIndex = 10;
+            label9.Text = "Rüzgar";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.Black;
+            label10.Location = new Point(21, 420);
+            label10.Name = "label10";
+            label10.Size = new Size(119, 38);
+            label10.TabIndex = 9;
+            label10.Text = "Durum";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CornflowerBlue;
+            ClientSize = new Size(520, 605);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.Crimson;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+    }
+}
---
+++ Hava_Durumu/Form1.cs
@@ -0,0 +1,49 @@
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace Hava_Durumu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text=DateTime.Now.ToShortDateString();
+
+            string api = "08f988e20ef31a7f953f07c97ef68d0c";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument hava = XDocument.Load(connection);
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var nem = hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+            var rüzgar = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var durum = hava.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+
+            label4.Text = sicaklik.ToString();
+            label6.Text = rüzgar.ToString();
+            label7.Text = nem.ToString();
+            label9.Text = durum.ToString();
+
+            double sicaklikDegeri = Convert.ToDouble(sicaklik, CultureInfo.InvariantCulture);
+
+            if (sicaklikDegeri <= 10)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\Mehmet Beþir\\Desktop\\depositphotos_425579014-stock-illustration-weather-overcast-snowing-icon-color.jpg";
+            }
+
+            else
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\Mehmet Beþir\\Desktop\\istockphoto-907865488-612x612.jpg";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
