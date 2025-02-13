180669de4d29fe55958adabb6f6f2b364338350e
besirr <besiryilmaz99@gmail.com>
2025-02-13 08:19:19 +0300

    Update Form1.cs

--- Hava_Durumu/Form1.cs
+++ Hava_Durumu/Form1.cs
@@ -14,16 +14,16 @@ namespace Hava_Durumu
         {
             label2.Text=DateTime.Now.ToShortDateString();
 
-            string api = "08f988e20ef31a7f953f07c97ef68d0c";
+            string api = "xxxx";
             string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
             XDocument hava = XDocument.Load(connection);
             var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             var nem = hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
-            var rüzgar = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var rÃ¼zgar = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
             var durum = hava.Descendants("clouds").ElementAt(0).Attribute("name").Value;
 
             label4.Text = sicaklik.ToString();
-            label6.Text = rüzgar.ToString();
+            label6.Text = rÃ¼zgar.ToString();
             label7.Text = nem.ToString();
             label9.Text = durum.ToString();
 
@@ -31,12 +31,12 @@ namespace Hava_Durumu
 
             if (sicaklikDegeri <= 10)
             {
-                pictureBox1.ImageLocation = "C:\\Users\\Mehmet Beþir\\Desktop\\depositphotos_425579014-stock-illustration-weather-overcast-snowing-icon-color.jpg";
+                pictureBox1.ImageLocation = "C:\\Users\\Mehmet BeÃ¾ir\\Desktop\\depositphotos_425579014-stock-illustration-weather-overcast-snowing-icon-color.jpg";
             }
 
             else
             {
-                pictureBox1.ImageLocation = "C:\\Users\\Mehmet Beþir\\Desktop\\istockphoto-907865488-612x612.jpg";
+                pictureBox1.ImageLocation = "C:\\Users\\Mehmet BeÃ¾ir\\Desktop\\istockphoto-907865488-612x612.jpg";
             }
         }
 
